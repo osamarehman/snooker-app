8432511993effc1ee7faeb915d06e40fe512a229
Usama Rehman <osamarehman@live.co.uk>
2025-02-15 18:56:44 +0500

    Update page.tsx

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -3,14 +3,7 @@ import { LoginForm } from "@/components/auth/LoginForm"
 export default function LoginPage() {
   return (
     <>
-      <div className="flex flex-col space-y-2 text-center">
-        <h1 className="text-2xl font-semibold tracking-tight">
-          Welcome back
-        </h1>
-        <p className="text-sm text-muted-foreground">
-          Enter your email and password to sign in to your account
-        </p>
-      </div>
+     
       <LoginForm />
     </>
   )
